c432516c193086b4bbb71e5dbc75094bb91924c9
tkddnjs365 <tkddnjs365@naver.com>
2025-08-19 17:41:13 +0900

    거래처관리 개발

--- src/utils/utilsUrl.ts
+++ src/utils/utilsUrl.ts
@@ -1,5 +1,5 @@
 const utilsUrl = {
-     //REST_API_URL: "/api"
-    REST_API_URL: "http://localhost:7010/api"
+     REST_API_URL: "/api"
+    //REST_API_URL: "http://localhost:7010/api"
 };
 export default utilsUrl;
